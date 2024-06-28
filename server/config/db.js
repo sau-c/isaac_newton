b4e1153a1116d488ce2b0222537c88485f7f301c
sau_c <sauc0202@gmail.com>
2024-06-28 11:15:21 -0600

    Resolver conflictos

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,14 @@
 const mongoose = require('mongoose');
-
-const connectDB = async () => {
+const connectDB = async()=> {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // Termina el proceso si no se puede conectar
+    console.log(error);
   }
-};
+}
+
 
 
 module.exports = connectDB;
